4e61de6155d18cc3f9a260cee698f344410cbbe5
Valerio Vaz <valerio@focusmedia.pt>
2022-01-01 12:51:12 +0000

    ignore files and setup variables

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # create backup folders
-echo "Creating backup folders"
-cd .. && mkdir backups && mkdir backups/db/
-echo "DONE"
+# echo "Creating backup folders"
+# cd .. && mkdir backups && mkdir backups/db/
+# echo "DONE"
 
+# getting variables
+echo "Whats your site name on LOCAL?"
+read siteName
+# echo "name is $siteName"
+echo "siteName=$siteName" > variables.dat
