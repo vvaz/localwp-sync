32ee67d75c0be77efd6cf580dd3c79fe706d1587
vvaz <valerio@wpbox.io>
2024-07-14 00:36:13 +0100

    update readme

--- files-sync.sh
+++ files-sync.sh
@@ -7,7 +7,7 @@ username=$(grep "username:" "$conf_file" | awk '{print $2}') # username
 server=$(grep "server:" "$conf_file" | awk '{print $2}') # IP address of the server
 app_name=$(grep "app_name:" "$conf_file" | awk '{print $2}') # app name
 live_domain=$(grep "live_domain:" "$conf_file" | awk '{print $2}') # live domain
-dev_domain=https://impakttv.ddev.site # dev domain
+dev_domain=https://merch.getquick.io.ddev.site # dev domain
 ssh_key=$(grep "ssh_key:" "$conf_file" | awk '{print $2}') # ssh key
 
 
